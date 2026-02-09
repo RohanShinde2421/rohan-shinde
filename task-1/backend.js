2e819c5c8fac3f1bd19da22bdc43ef8002d5bd18
RohanShinde2421 <shinderohan24212gmail.com>
2026-02-09 07:17:12 +0000

    Updated

--- task-1/backend.js
+++ task-1/backend.js
@@ -4,7 +4,6 @@ const mysql = require("mysql2");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
-// MySQL connection
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -12,16 +11,14 @@ const db = mysql.createConnection({
   database: "task1db"
 });
 
-// DB connect
 db.connect((err) => {
   if (err) {
-    console.log("DB Connection Failed ❌");
+    console.log("DB Connection Failed");
     return;
   }
-  console.log("DB Connected Successfully ✅");
+  console.log("DB Connected Successfully");
 });
 
-// ✅ GET route (browser साठी)
 app.get("/", (req, res) => {
   res.send(`
     <h2>Task 1 – Simple App</h2>
@@ -32,18 +29,16 @@ app.get("/", (req, res) => {
   `);
 });
 
-// POST route
 app.post("/add", (req, res) => {
   db.query(
     "INSERT INTO users (name) VALUES (?)",
     [req.body.name],
     () => {
-      res.send("User Added Successfully ✅ <br><a href='/'>Go Back</a>");
+      res.send("User Added Successfully  <br><a href='/'>Go Back</a>");
     }
   );
 });
 
-// ✅ IMPORTANT: listen on 0.0.0.0
 app.listen(3000, "0.0.0.0", () => {
   console.log("Server running on port 3000");
 });
